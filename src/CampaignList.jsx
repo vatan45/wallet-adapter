39016c112d5c60c4f469cb2d1813f443a99925f7
vatan malik <vmro45@gmail.com>
2024-09-01 21:52:24 +0530

    changed urls

--- src/CampaignList.jsx
+++ src/CampaignList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 const CampaignList = () => {
     const [campaigns, setCampaigns] = useState([]);
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios']
-    }
-  }
+      external: [], // Ensure `axios` is not listed here
+    },
+  },
 });
