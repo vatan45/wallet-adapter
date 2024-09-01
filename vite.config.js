0a75663e6cda31ce78c5ea7aa1eaed6b8d3f8c05
vatan malik <vmro45@gmail.com>
2024-09-01 14:50:48 +0530

    build setup

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';  // Use named import here
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['buffer', 'crypto', 'stream', 'util'],
-    }),
-  ],
-  define: {
-    'process.env': {},
-  },
+  plugins: [react()],
 });
